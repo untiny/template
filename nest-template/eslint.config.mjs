cb43c5f8c4aa78aea61478fe44d0d6eb616f8482
untiny <837233287@qq.com>
2025-05-22 14:58:06 +0800

    chore: update .prettierrc and refactor eslint.config.mjs for improved formatting and organization

--- nest-template/eslint.config.mjs
+++ nest-template/eslint.config.mjs
@@ -1,31 +1,27 @@
-import antfu from '@antfu/eslint-config';
-import globals from 'globals';
+import antfu from '@antfu/eslint-config'
+import globals from 'globals'
 
-export default antfu(
-  {
-    typescript: true,
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },   
-    rules: {
-      'node/prefer-global/process': 'off',
-      'ts/consistent-type-imports': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
+export default antfu({
+  typescript: true,
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.jest,
     },
-    ignores: [
-      'src/generated/**/*',
-    ]
+    ecmaVersion: 5,
+    sourceType: 'module',
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
-);
+  rules: {
+    'node/prefer-global/process': 'off',
+    'ts/consistent-type-imports': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-unsafe-argument': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignores: ['src/generated/**/*'],
+})
