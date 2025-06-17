2428641d38da6b58416bd5a147ba154b09665373
untiny <837233287@qq.com>
2025-06-17 20:30:24 +0800

    chore: 移除不必要的 @Render 装饰器，简化 getHello 方法

--- nest-template/src/app.controller.ts
+++ nest-template/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { ClsService } from 'nestjs-cls'
 import { AppService } from './app.service'
@@ -9,7 +9,6 @@ export class AppController {
   constructor(private readonly appService: AppService, private readonly cls: ClsService) {}
 
   @Get()
-  @Render('index')
   getHello() {
     return {
       message: 'Hello World!',
